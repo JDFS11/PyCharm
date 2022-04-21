cad8da5a36c4c6f226140a7671d73f82e663de6e
Julian Feria <julian.david.feria@gmail.com>
2022-04-21 16:01:24 -0500

    Quinto commit

--- Pruebas.py
+++ Pruebas.py
@@ -7,3 +7,4 @@ X = [i**2 for i in range(5)]
 print(X)
 print("Hello Julian, esta es la primera modificacion en Pycharm")
 print("Anucio para el cuarto caommit")
+print("Anucio para el cuarto commit")
